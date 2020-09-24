13d588800085e76f53d239ba277d0cbb5e517e24
kevmodrome <kevin@kevmodro.me>
2020-09-24 10:44:38 +0200

    first commit

---
+++ src/Component.svelte
@@ -0,0 +1,54 @@
+<script>
+  import { Grid } from "ag-grid-community";
+  import { onMount } from "svelte";
+
+  export let columnDefs;
+  export let data;
+  export let options = {
+    columnDefs,
+    defaultColDef: {
+      flex: 1,
+      minWidth: 150,
+      filter: true,
+    },
+    rowData: data,
+  };
+
+  let ref;
+  let grid;
+  let api;
+
+  const onCellValueChanged = (e) => {
+    data[e.rowIndex] = e.data;
+  };
+
+  const onGridReady = () => {
+    api = grid.gridOptions.api;
+  };
+
+  const updateData = (data) => {
+    if (grid && api) {
+      api.setRowData(data);
+    }
+  };
+
+  onMount(() => {
+    grid = new Grid(ref, { ...options, onGridReady, onCellValueChanged });
+  });
+
+  $: updateData(data);
+</script>
+
+<style>
+  .container {
+    width: 100%;
+    height: var(--grid-height, 800px);
+  }
+</style>
+
+<div class="container">
+  <div
+    bind:this={ref}
+    style="height: 100%; width:100%"
+    class="ag-theme-alpine" />
+</div>
