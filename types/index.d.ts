de9071d1f95e69a749c9f8671940780ffe82b5f0
michmich112 <cantacuzene.michel@gmail.com>
2021-10-03 12:34:04 -0700

    added types for AgGridComponent

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,12 +1,21 @@
-import {SvelteComponentTyped} from "svelte";
-// import {} from "ag-grid-community"
+import { SvelteComponent } from "svelte";
 
-interface AgGridProps {
-    columnDefs: any;
-    data: any;
-    theme?: string;
-    options?: any;
-    loading?: boolean;
-}
+/* TODO: Use when updated to Svelte ^3.31.0 with SvelteComponentTyped */
+/**
+ * import { ColDef, GridOptions } from "ag-grid-community"
+ *
+ * interface AgGridProps {
+ *  columnDefs: ColDef;
+ *  data: any;
+ *  theme?: string;
+ *  options?: GridOptions;
+ *  loading?: boolean;
+ * }
+ *
+ * declare class AgGridComponent extends SvelteComponentTyped<AgGridProps> {}
+*/
 
-export class AgGrid extends SvelteComponentTyped<AgGridProps>{};
+declare class AgGridComponent extends SvelteComponent { }
+
+export default AgGridComponent;
+
